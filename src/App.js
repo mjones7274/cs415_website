890b9b5afa2151ab13d85a7038a5010b8357293e
mjones7274 <mdj7274@gmail.com>
2024-02-22 11:55:22 -0700

    Added new modules to pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import UserView from "./Components/UserView";
 import Module1 from "./Components/Module1";
 import Module2 from "./Components/Module2";
 import Module3 from "./Components/Module3";
+import Module4 from "./Components/Module4";
+import Module5 from "./Components/Module5";
+import Module6 from "./Components/Module6";
 
 function NoMatch() {
   return (
@@ -44,6 +47,9 @@ function App() {
             <Link to="/modules/1" style={{ padding: 5 }}>Module 1</Link>
             <Link to="/modules/2" style={{ padding: 5 }}>Module 2</Link>
             <Link to="/modules/3" style={{ padding: 5 }}>Module 3</Link>
+            <Link to="/modules/4" style={{ padding: 5 }}>Module 4</Link>
+            <Link to="/modules/5" style={{ padding: 5 }}>Module 5</Link>
+            <Link to="/modules/6" style={{ padding: 5 }}>Module 6</Link>
           </nav>
           <div className="App">
           <Routes>
@@ -55,6 +61,9 @@ function App() {
             <Route path="/modules/1" element={<Module1 />} />
             <Route path="/modules/2" element={<Module2 />} />
             <Route path="/modules/3" element={<Module3 />} />
+            <Route path="/modules/4" element={<Module4 />} />
+            <Route path="/modules/5" element={<Module5 />} />
+            <Route path="/modules/6" element={<Module6 />} />
             <Route path="/unauthorized" element={<UnAuthorized />} />
             <Route path="*" element={<NoMatch />} />
           </Routes>
---
+++ src/Components/Module5.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from "react-router-dom"
+
+const Module5 = () => {
+  const navigate = useNavigate();
+  const [pageName, setPageName] = useState('')
+  const [pageTitle, setPageTitle] = useState('')
+  const [pageDescription, setPageDescription] = useState('')
+
+  useEffect(() => {
+    if (!window.sessionStorage.getItem("auth")) navigate('/unauthorized')
+    fetch(process.env.REACT_APP_API_URL_BASE + '/pages/page/5')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        setPageName(data.page.page_name)
+        setPageTitle(data.page.page_title)
+        setPageDescription(data.page.page_description)
+    })
+    .catch(error => console.error(error));
+}, []);
+
+  return (
+    <div>
+
+      <h1>CS415 Private Page - Module 5</h1>
+      <p><font color="white">This is only accessible when logged in</font></p>
+
+        <h2>{pageName}</h2>
+        <p><b>{pageTitle}</b></p>
+        <p><font color='white'>{pageDescription}</font></p>
+    </div>
+
+  )
+}
+
+export default Module5
